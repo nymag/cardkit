5c71af597078676d2d8a0a7e66e3e662f45018f5
Nelson Pecora <nelson@yoshokatana.com>
2015-09-18 17:53:01 -0400

    added smart quotes

--- app/scripts/directives/texteditor.js
+++ app/scripts/directives/texteditor.js
@@ -1,5 +1,20 @@
 'use strict';
 
+function smarten(text) {
+  return text
+    .replace(/(\W|^)"(\S)/g, '$1\u201c$2') // beginning "
+    .replace(/(\u201c[^"]*)"([^"]*$|[^\u201c"]*\u201c)/g, '$1\u201d$2') // ending "
+    .replace(/([^0-9])"/g,'$1\u201d') // remaining " at end of word
+    .replace(/(\W|^)'(\S)/g, '$1\u2018$2') // beginning '
+    .replace(/([a-z])'([a-z])/ig, '$1\u2019$2') // conjunction's possession
+    .replace(/((\u2018[^']*)|[a-z])'([^0-9]|$)/ig, '$1\u2019$3') // ending '
+    .replace(/(\u2018)([0-9]{2}[^\u2019]*)(\u2018([^0-9]|$)|$|\u2019[a-z])/ig, '\u2019$2$3') // abbrev. years like '93
+    .replace(/(\B|^)\u2018(?=([^\u2019]*\u2019\b)*([^\u2019\u2018]*\W[\u2019\u2018]\b|[^\u2019\u2018]*$))/ig, '$1\u2019') // backwards apostrophe
+    .replace(/'''/g, '\u2034') // triple prime
+    .replace(/("|'')/g, '\u2033') // double prime
+    .replace(/'/g, '\u2032'); // prime
+}
+
 /**
  * @ngdoc directive
  * @name cardkitApp.directive:textEditor
@@ -9,11 +24,19 @@
 angular.module('cardkitApp')
   .directive('textEditor', function () {
     return {
-      template: '<div><label>Text</label><textarea ng-model="element.text" class="form-control"></textarea></div>',
+      template: '<div><label>Text (click away for smart quotes)</label><textarea ng-model="element.text" class="form-control"></textarea></div>',
       restrict: 'E',
       replace: true,
   	  scope: {
   	    element: '='
-  	  }
+  	  },
+      link: function (scope, el) {
+        var textArea = el[0].querySelector('textarea');
+
+        textArea.addEventListener('blur', function () {
+          scope.element.text = smarten(scope.element.text);
+          scope.$apply();
+        });
+      }
     };
   });
